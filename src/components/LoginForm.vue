48aed54cdf97a6bf0df322d30a77b915d4b5954c
Marc Heintz <heintz.marc@web.de>
2023-03-21 00:31:31 +0100

    einheitliches Login & Registrierungsdesign

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,15 +1,27 @@
 <template>
-  <form @submit.prevent="loginUser">
-    <div>
-      <label for="email">E-Mail</label>
-      <input type="email" id="email" v-model="email" required>
+
+
+  <div class="background container">
+        <div class="shape"></div>
+        <div class="shape"></div>
     </div>
-    <div>
-      <label for="password">Passwort</label>
-      <input type="password" id="password" v-model="password" required>
-    </div>
-    <button type="submit">Anmelden</button>
-  </form>
+    <form @submit.prevent="loginUser">
+        <h3>Login</h3>
+
+        <label for="email">E-Mail Adresse</label>
+        <input type="email" placeholder="Email" v-model="email" id="username">
+
+        <label for="password">Passwort:</label>
+        <input type="password" id="password" placeholder="Passwort" v-model="password" required>
+
+        <button>Anmelden</button>
+        <div class="mt-3 already-registrated">
+        <p>Zur Registrierung ➜ <span id="login-id" @click="enterRegistrationView">Registrierung</span></p>
+        </div>
+    </form>
+
+
+
 </template>
 
 <script>
@@ -42,67 +54,153 @@ export default {
       }
     }
 
-    return { email, password, loginUser }
+    const enterRegistrationView = () =>{
+      router.push('/')
+    }
+
+
+    return { email, password, loginUser, enterRegistrationView }
   }
 }
 </script>
 <style scoped>
-form {
+*,
+*:before,
+*:after{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+.container{
+    background-color: #080710;
+}
+.background{
+    width: 430px;
+    height: 520px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    left: 50%;
+    top: 50%;
+}
+.background .shape{
+    height: 200px;
+    width: 200px;
+    position: absolute;
+    border-radius: 50%;
+}
+.shape:first-child{
+    background: linear-gradient(
+        #1845ad,
+        #23a2f6
+    );
+    left: -80px;
+    top: -80px;
+}
+.shape:last-child{
+    background: linear-gradient(
+        to right,
+        #ff512f,
+        #f09819
+    );
+    right: -30px;
+    bottom: -80px;
+}
+form{
+    height: 520px;
+    width: 400px;
+    background-color: rgba(255,255,255,0.13);
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255,255,255,0.1);
+    box-shadow: 0 0 40px rgba(8,7,16,0.6);
+    padding: 50px 35px;
+}
+form *{
+    font-family: 'Poppins',sans-serif;
+    color: #ffffff;
+    letter-spacing: 0.5px;
+    outline: none;
+    border: none;
+}
+form h3{
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 42px;
+    text-align: center;
+}
+
+label{
+    display: block;
+    margin-top: 30px;
+    font-size: 16px;
+    font-weight: 500;
+}
+input{
+    display: block;
+    height: 50px;
+    width: 100%;
+    background-color: rgba(255,255,255,0.07);
+    border-radius: 3px;
+    padding: 0 10px;
+    margin-top: 8px;
+    font-size: 14px;
+    font-weight: 300;
+}
+::placeholder{
+    color: #e5e5e5;
+}
+button{
+    margin-top: 50px;
+    width: 100%;
+    background-color: #ffffff;
+    color: #080710;
+    padding: 15px 0;
+    font-size: 18px;
+    font-weight: 600;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.social{
+  margin-top: 30px;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 0 auto;
-  max-width: 400px;
-  padding: 1rem;
-  border-radius: 8px;
-  background-color: #f5f5f5;
-  box-shadow: 0 4px 8px rgba(30, 30, 30, 0.1);
+}
+.social div{
+  background: red;
+  width: 150px;
+  border-radius: 3px;
+  padding: 5px 10px 10px 5px;
+  background-color: rgba(255,255,255,0.27);
+  color: #eaf0fb;
+  text-align: center;
+}
+.social div:hover{
+  background-color: rgba(255,255,255,0.47);
+}
+.social .fb{
+  margin-left: 25px;
+}
+.social i{
+  margin-right: 4px;
 }
 
-div {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 1rem;
-  width: 100%;
-}
-
-label {
-  font-size: 1.1rem;
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-  color: #333;
-}
-
-input {
-  border: none;
-  border-radius: 4px;
-  padding: 0.5rem;
-  font-size: 1rem;
-  width: 100%;
-  background-color: #fff;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  transition: box-shadow 0.2s ease-in-out;
-}
-
-input:focus {
-  outline: none;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  border: none;
-  border-radius: 4px;
-  padding: 0.5rem 1rem;
-  font-size: 1.1rem;
-  font-weight: bold;
-  color: #fff;
-  background-color: #00a8cc;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  transition: background-color 0.2s ease-in-out;
-}
-
-button:hover {
+#login-id{
   cursor: pointer;
-  background-color: #008da6;
+  font-size: 19px;
+  text-decoration: underline;
+  text-underline-position: below;
+  text-decoration-style: solid;
 }
+
+.already-registrated{
+  text-align: center;
+}
+
+
+
+
+
 </style>
--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="registration-form">
-    <h1>Registrierung</h1>
-    <form @submit.prevent="registerUser">
-      <div class="form-group">
-        <label for="email">E-Mail-Adresse:</label>
-        <input type="email" id="email" v-model="email" required>
-      </div>
-      <div class="form-group">
+ 
+ <div class="background container">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form>
+        <h3>Registrierung</h3>
+
+        <label for="email">E-Mail Adresse</label>
+        <input type="email" placeholder="Email" v-model="email" id="username">
+
         <label for="password">Passwort:</label>
-        <input type="password" id="password" v-model="password" required>
-      </div>
-      <div class="form-group">
-        <button type="submit">Registrieren</button>
-      </div>
-        <p>Bereits registriert? ➜ <span id="Login-Id" @click="enterLoginView">Login</span></p>
+        <input type="password" id="password" placeholder="Passwort" v-model="password" required>
+
+        <button>Registrieren</button>
+        <div class="mt-3 already-registrated">
+        <p>Bereits registriert? ➜ <span id="login-id" @click="enterLoginView">Login</span></p>
+        </div>
     </form>
-  </div>
+
 </template>
 
 <script>
@@ -60,67 +63,139 @@ export default {
 </script>
 <style scoped>
 
-#Login-Id:hover{
+*,
+*:before,
+*:after{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+.container{
+    background-color: #080710;
+}
+.background{
+    width: 430px;
+    height: 520px;
+    position: absolute;
+    transform: translate(-50%,-50%);
+    left: 50%;
+    top: 50%;
+}
+.background .shape{
+    height: 200px;
+    width: 200px;
+    position: absolute;
+    border-radius: 50%;
+}
+.shape:first-child{
+    background: linear-gradient(
+        #1845ad,
+        #23a2f6
+    );
+    left: -80px;
+    top: -80px;
+}
+.shape:last-child{
+    background: linear-gradient(
+        to right,
+        #ff512f,
+        #f09819
+    );
+    right: -30px;
+    bottom: -80px;
+}
+form{
+    height: 520px;
+    width: 400px;
+    background-color: rgba(255,255,255,0.13);
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255,255,255,0.1);
+    box-shadow: 0 0 40px rgba(8,7,16,0.6);
+    padding: 50px 35px;
+}
+form *{
+    font-family: 'Poppins',sans-serif;
+    color: #ffffff;
+    letter-spacing: 0.5px;
+    outline: none;
+    border: none;
+}
+form h3{
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 42px;
+    text-align: center;
+}
+
+label{
+    display: block;
+    margin-top: 30px;
+    font-size: 16px;
+    font-weight: 500;
+}
+input{
+    display: block;
+    height: 50px;
+    width: 100%;
+    background-color: rgba(255,255,255,0.07);
+    border-radius: 3px;
+    padding: 0 10px;
+    margin-top: 8px;
+    font-size: 14px;
+    font-weight: 300;
+}
+::placeholder{
+    color: #e5e5e5;
+}
+button{
+    margin-top: 50px;
+    width: 100%;
+    background-color: #ffffff;
+    color: #080710;
+    padding: 15px 0;
+    font-size: 18px;
+    font-weight: 600;
+    border-radius: 5px;
     cursor: pointer;
-    color:  green;
 }
-
-.registration-form {
+.social{
+  margin-top: 30px;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 80%;
+}
+.social div{
+  background: red;
+  width: 150px;
+  border-radius: 3px;
+  padding: 5px 10px 10px 5px;
+  background-color: rgba(255,255,255,0.27);
+  color: #eaf0fb;
+  text-align: center;
+}
+.social div:hover{
+  background-color: rgba(255,255,255,0.47);
+}
+.social .fb{
+  margin-left: 25px;
+}
+.social i{
+  margin-right: 4px;
 }
 
-h1 {
-  font-size: 2rem;
-  margin-bottom: 1.5rem;
+#login-id{
+  cursor: pointer;
+  font-size: 19px;
+  text-decoration: underline;
+  text-underline-position: below;
+  text-decoration-style: solid;
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 25rem;
-  background-color: #fff;
-  padding: 2rem;
-  border-radius: 0.5rem;
-  box-shadow: 0 0.25rem 1rem rgba(0, 0, 0, 0.2);
+.already-registrated{
+  text-align: center;
 }
 
-.form-group {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-bottom: 1.5rem;
-}
-
-label {
-  font-size: 1.2rem;
-  margin-bottom: 0.5rem;
-}
-
-input[type="email"],
-input[type="password"] {
-  width: 100%;
-  padding: 0.5rem;
-  border: none;
-  border-radius: 0.25rem;
-  box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.1);
-  transition: box-shadow 0.2s ease-in-out;
-}
-
-input[type="email"]:focus,
-input[type="password"]:focus {
-  outline: none;
-  box-shadow: 0 0.125rem 0.5rem rgba(0, 0, 0, 0.2);
-}
-
-button[type="submit"] {
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-}
 </style>
