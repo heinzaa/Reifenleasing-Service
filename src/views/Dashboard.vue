3cee60345ef962587d78aacdaf288091e4e5bf02
Marc Heintz <heintz.marc@web.de>
2023-03-25 00:28:25 +0100

    Testen des Berechnungsalgorithmus

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,14 +3,31 @@
 <p class="heading-text">Kundenübersicht</p>
 </div>
         <CustomerOverview />
+        <button @click="calculate">Calculate</button>
 </template>
 
 <script>
 import CustomerOverview from '../components/CustomerOverview.vue'
+import calculateLeasingPrice from '../composables/calculateLeasingPrice';
+import { ref } from 'vue'
 
 export default {
     components:{
         CustomerOverview
+    },
+
+    setup(){
+
+    const calculate = ref()
+    
+    const { price } = calculateLeasingPrice() 
+
+    return {
+
+        calculate
+
+    }
+
     }
 
 }
